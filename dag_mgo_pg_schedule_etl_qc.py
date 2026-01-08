ea7d8a2f19c573b51b9450fba826d5037639a553
Aung Zaw Htet <59396515+aungzawhtet-dev@users.noreply.github.com>
2026-01-08 07:42:01 +0630

    Update dag_mgo_pg_schedule_etl_qc.py

--- dag_mgo_pg_schedule_etl_qc.py
+++ dag_mgo_pg_schedule_etl_qc.py
@@ -166,7 +166,7 @@ def transform_task(**context):
     df = normalize_columns(df)
     # CamelCase / Snake_case change to lower letters
 
-    # handling nested fields from MongoDB to Json string to be ready for PostgreSQL as “one cell = one value” (not a structure, not a collection).
+    # handling nested fields from MongoDB stores documents as BSON (Binary JSON) to Json string to be ready for PostgreSQL as “one cell = one value” (not a structure, not a collection).
     for col in ["requestparams", "createdby", "updatedby", "statuschangedby"]:
         if col in df.columns:
             df[col] = df[col].apply(clean_value)
@@ -476,4 +476,5 @@ with DAG(
     
     
     check_conn >> extract_task >> transform >> load >> soda_quality_check >> row_count_check
+
 
